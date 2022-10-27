ca0166124ae79f8a076c8d92b0e6ef0ef4955a92
Alena1308 <Lenamironova13@mail.ru>
2022-10-28 00:00:15 +0300

    finish

---
+++ src/course1/objects_and_classes/Author.java
@@ -0,0 +1,16 @@
+package course1.objects_and_classes;
+
+public class Author {
+    private String firstNameAuthor;
+    private String lastNameAuthor;
+    public Author(String firstNameAuthor, String lastNameAuthor) {
+        this.firstNameAuthor = firstNameAuthor;
+        this.lastNameAuthor = lastNameAuthor;
+    }
+    public String getFirstNameAuthor () {
+        return this.firstNameAuthor;
+    }
+    public String getLastNameAuthor () {
+        return this.lastNameAuthor;
+    }
+}
--- src/course1/objects_and_classes/Main.java
+++ src/course1/objects_and_classes/Main.java
@@ -1,2 +1,35 @@
-package course1.objects_and_classes;public class Main {
+package course1.objects_and_classes;
+
+public class Main {
+    public static void main(java.lang.String[] args) {
+        Author exupery = new Author("Antonie", "de Saint-Exupery");
+        Book book = new Book("The Little Prince", exupery, 1942);
+        System.out.println("name = " + book.getName());
+        System.out.println("author = " + exupery.getFirstNameAuthor() + " " + exupery.getLastNameAuthor());
+        System.out.println("year = " + book.getYear());
+        book.setYear(2018);
+        System.out.println("book.getYear() = " + book.getYear());
+
+        Book book1 = new Book ("Nightt flight", exupery, 1930);
+        System.out.println("name = " + book1.getName());
+        System.out.println("author = " + exupery.getFirstNameAuthor() + " " + exupery.getLastNameAuthor());
+        System.out.println("year = " + book1.getYear());
+        book1.setYear(1976);
+        System.out.println("book1.getYear() = " + book1.getYear());
+
+        Author braun = new Author("Ден", "Браун");
+        Book book2 = new Book("Цифровая крепоость", braun, 2017);
+        System.out.println("name = " + book2.getName());
+        System.out.println("author = " + braun.getFirstNameAuthor() + " " + braun.getLastNameAuthor());
+        System.out.println("year = " + book2.getYear());
+        book2.setYear(2020);
+        System.out.println("book2.getYear() = " + book2.getYear());
+
+        Book book3 = new Book("Утраченный символ", braun, 2009);
+        System.out.println("name = " + book3.getName());
+        System.out.println("author = " + braun.getFirstNameAuthor() + " " + braun.getLastNameAuthor());
+        System.out.println("year = " + book3.getYear());
+        book3.setYear(2021);
+        System.out.println("book3.getYear() = " + book3.getYear());
+    }
 }
